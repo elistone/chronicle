343af3a3fcdc8c546f0cdc4faead74551f5ca5cc
Eli Stone <2546188+elistone@users.noreply.github.com>
2026-06-21 13:09:50 +0100

    Initial Commit

---
+++ Chronicle/ChronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicleApp.swift
+//  Chronicle
+//
+//  Created by Eli Stone on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chronicle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chronicle
+//
+//  Created by Eli Stone on 21/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
